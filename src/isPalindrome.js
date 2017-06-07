66c3127f94b1465e52401a527e7541318164ff4d
aaronev <aarone.villanueva@gmail.com>
2017-06-06 17:36:27 -0700

    refactored isPalindrom and makeChange

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -1,5 +1,5 @@
 export default function isPalindrome(str) {
   let string = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
   let revStr = string.split("").reverse().join("");
-  return string === revStr ? true : false
+  return string === revStr
 }
--- src/makeChange.js
+++ src/makeChange.js
@@ -1,12 +1,8 @@
 export default function makeChange(price, amountGiven) {
   let change = amountGiven - price, coins = {}
-  coins.quarters = Math.floor(change / 25)
-    change -= (coins.quarters * 25)
-  coins.dimes = Math.floor(change / 10)
-    change -= (coins.dimes * 10)
-  coins.nickels = Math.floor(change / 5)
-    change -= (coins.nickels * 5)
-  coins.pennies = Math.floor(change / 1)
-    change -= (coins.pennies * 1)
+  change -= (coins.quarters = Math.floor(change / 25)) * 25
+  change -= (coins.dimes = Math.floor(change / 10)) * 10
+  change -= (coins.nickels = Math.floor(change / 5)) * 5
+  coins.pennies = change
   return coins
 }
