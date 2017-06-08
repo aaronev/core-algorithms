dd5b3a7ba445c8ce3b92266cd10f79ac065da7d0
aaronev <aarone.villanueva@gmail.com>
2017-06-08 14:01:37 -0700

    reworded isConnectedGraph for clarity

--- src/isConnectedGraph.js
+++ src/isConnectedGraph.js
@@ -1,10 +1,10 @@
 export default function isConnectedGraph(graph) {
   var keys = Object.keys(graph)
-  var minPairs = (keys.length - 1) * 2
+  var minimunConnections = (keys.length - 1) * 2
   var connects = 0
   var point = Object.values(graph)
   for (var vert in graph) {
     point.map(function(point){ if (point.includes(vert)) connects++ } )
   }
-  return connects >= minPairs
+  return connects >= minimunConnections
 }
